685ba65f3a6b731e6fb17806b4c3d9ded4f746c5
Rita Nayak <64993443+ritanayak@users.noreply.github.com>
2026-03-31 11:19:45 -0400

    refactor: move mongodb connection logic to config/db.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import projectRoutes from './routes/projectRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
+import connectDB from "./config/db.js";
 
 
 dotenv.config();
+
+connectDB();
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then (()=> console.log ("DB connected"));
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
-app.get('/', (req, res)=>{
-    res.send("API running")
-
-})
-
-app.listen (process.env.PORT || 3000, () =>{
-
-    console.log("Server running");
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
